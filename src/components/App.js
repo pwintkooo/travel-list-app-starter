97a8083e83ced5b2e022da6c164d4915db0b5676
pwintkooo <23029589@myrp.edu.sg>
2024-12-01 20:26:14 +0800

    L6Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,62 @@
-// Initial packing items
-const initialItems = [
-  { id: 1, description: "Shirt", quantity: 5, packed: false },
-  { id: 2, description: "Pants", quantity: 2, packed: false },
-];
+import React, {useState } from "react";
 
 function Logo() {
   return <h1>My Travel List</h1>;
 }
 
-function Form() {
+function Form({handleAddItems}) {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+
+    const newItem = {
+      id: Date.now(),
+      description: description,
+      quantity: quantity,
+      packed: false
+    };
+
+    handleAddItems(newItem);
+    setDescription('');
+    setQuantity(1);
+  }
+
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need to pack?</h3>
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+      </select>
+      <input placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)}/>
+      <button>ADD</button>
     </form>
   );
 }
 
-function PackingList() {
+function PackingList({items}) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <li>{item.description}</li>
+        {items.map((item) => (
+          <Item item={item} key={item.id}/>
         ))}
       </ul>
     </div>
   );
+}
+
+function Item({item}) {
+  return(
+    <li style={{textDecoration: item.packed ? "line-through" : "none"}}>
+      {item.description} ({item.quantity})
+    </li>
+  )
 }
 
 function Stats() {
@@ -37,11 +68,17 @@ function Stats() {
 }
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((prevItems) => [...prevItems, item])
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form handleAddItems = {handleAddItems}/>
+      <PackingList items={items}/>
       <Stats />
     </div>
   );
